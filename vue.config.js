503542e73303ddd0464623eed4c8ffd6057385e0
Kenny <kboamponsem@syncline.it>
2020-12-12 13:34:30 +0000

    hail mary build II

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	publicPath:
-		process.env.NODE_ENV === "production" ? "/alan-news/dist" : "/dist",
+		process.env.NODE_ENV === "production"
+			? "/alan-news.github.io/dist/"
+			: "/dist",
 };
