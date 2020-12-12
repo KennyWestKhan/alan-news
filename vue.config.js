f682bd2c919e498b5962d199175c4508c05b2116
Kenny <kboamponsem@syncline.it>
2020-12-12 13:30:07 +0000

    hail mary fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/alan-news/" : "/",
+	publicPath:
+		process.env.NODE_ENV === "production" ? "/alan-news/dist" : "/dist",
 };
